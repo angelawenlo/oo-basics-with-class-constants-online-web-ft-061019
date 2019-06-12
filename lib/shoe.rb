43e2d6a88f8aca820d0c414cf06fa52010e7088d
Angela Lo <angelawenlo@gmail.com>
2019-06-12 20:16:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS =[]
+  ALL_BRANDS =[]
   BRANDS.uniq
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+     << brand
   end
 
   def cobble

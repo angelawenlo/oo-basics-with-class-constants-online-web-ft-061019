894cf6a23a3ee5289476531abcada1787f51917e
Angela Lo <angelawenlo@gmail.com>
2019-06-12 20:22:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,11 +5,12 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def brand=(brand)
     @brand = brand
-    BRANDS << brand
+
   end
 
   def cobble
